a67c6c780abf4363325342036bba105373ace8a4
arthurroberto20 <arthurroberto20@gmail.com>
2018-10-27 15:32:06 -0300

    Update index.php
    sticky menu

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ include 'layout_foot.php';
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <!-- navbar -->
-<div class="navbar navbar-default navbar-static-top" role="navigation">
+<div class="sicker-menu navbar navbar-default navbar-static-top" role="navigation">
     <div class="container-fluid">
  
         <div class="navbar-header">
